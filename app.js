69849e19f62847b894eb935b662c8928e1bd62f1
munsra <piero.silvestri93@gmail.com>
2017-04-28 16:03:42 +0200

    Aggiunto un casin de roba

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var mysql = require('mysql');
 
 //
 // BOT SETUP
@@ -7,7 +8,17 @@ var builder = require('botbuilder');
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function(){
+	console.log("Starting server");
 	console.log('%s listening to %s', server.name, server.url);
+});
+
+// Database connection
+var connection = mysql.createPool({
+	host: 'prohosting3.netsons.net',
+	port: '3306',
+	user: 'mjhnxzbg_test',
+	password: '@Password1.',
+	database: 'mjhnxzbg_test1'
 });
 
 //Create chat bot
@@ -15,17 +26,67 @@ var connector = new builder.ChatConnector({
 	appId: 'a11ec4b5-b7c1-4e52-accc-97a9efebed7d',
 	appPassword: 'nybp2a71m6bVWrX6JVX18MU'
 });
+
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
+
+// LUIS
+/**/
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/cf7b0942-055c-4111-87bf-d831a1725efa?subscription-key=728ad137437444a6b90797a5f388f685&timezoneOffset=0&verbose=true&q=';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', dialog);
+
+
+// Add intent handlers
+dialog.matches('BestTeam', [
+	function (session, args, next) {
+        // Resolve and store any entities passed from LUIS.
+        connection.getConnection(function(error, tempcont){
+		if(error){
+			//tempcont.release();
+			console.log('Error');
+			console.log(error);
+			session.send('Errore.');
+
+		}else{
+			console.log('Connected! WELL DONE!');
+			var query = "SELECT * FROM mySampleTable";
+
+			tempcont.query(query, function(error, rows, fields){
+
+				tempcont.release();
+
+				if(!!error){
+					console.log('Error in the query');
+					console.log(error)
+					session.send('Errore.');
+				}else{
+					if(rows.length == 0){
+						session.send('Errore.');
+					}
+					var utente = rows[0];
+					session.send("Il migliore è " + utente.NAME);
+				}
+			})
+		}
+	});
+        }
+    ]);
+dialog.matches('builtin.intent.alarm.delete_alarm', builder.DialogAction.send('Deleting Alarm'));
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
+
 
 process.on('uncaughtException', function (err) {
     console.log(err);
 }); 
 
+
 //
 // BOT DIALOGS
 //
-
+/*
 bot.dialog('/', function(session){
 	session.send("Hello World! Ciao a tutti :)");
 });
+*/
